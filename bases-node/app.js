d80445274cd9028fb26f3aba60c79c191bcf00cf
Kam <camiloandresarevaloc@hotmail.com>
2021-03-08 09:00:34 -0500

    update

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,18 +1,12 @@
-const fs = require('fs'); // hacemos el require al filesistem
-
-const base = 8;
-let salida = '';
+const {crearArchivo} = require('./helpers/multiplicar');
 
 console.clear(); // limpia consola antes de ejecutar codigo
 
-console.log('=====================')
-console.log(`la tabla del ${ base }`)
-console.log('=====================')
 
-for (let i = 1; i <= 10; i++) {    salida += `${base} x ${i} = ${base * i} \n`
-}
-console.log(salida)
-fs.writeFile(`tabla-${base}.txt`, salida, (err) =>{
-    if(err) throw err;
-    console.log(`La tabla del ${base} fue creada`);
-})
+const[,,arg3 = 'base=4'] = process.argv;
+const[, base = 5 ] = arg3.split('=');
+console.log(base);
+
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo,'fue creado'))
+console.log(1 + "1");
