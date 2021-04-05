f3001a5012a169d5137b6e0d28ba10462f9f04af
camilo <camiloandresarevaloc@hotmail.com>
2021-04-05 16:19:34 -0500

    En esta sección hemos usado el fs.readFile para leer la data que se encuentra dentro del archivo json

--- app-tareas/app.js
+++ app-tareas/app.js
@@ -1,6 +1,6 @@
 require('colors');
-const { guardarDb } = require('./helpers/guardarArchivo');
-const { inquirerMenu, pausa , leerInput} = require('./helpers/inquirer'); // importando la función de inquireMenu 
+const { guardarDb, leerDb } = require('./helpers/guardarArchivo');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer'); // importando la función de inquireMenu 
 const Tarea = require('./models/tarea');
 const Tareas = require('./models/tareas');
 
@@ -12,22 +12,27 @@ let opt = '';
 const tareas = new Tareas(); // inicializando las tareas
 const main = async () => { // utilizamos el async ya que al implementar varias funciónes estas deberán de ejecutarse en secuencia
 
+    const tareasDb = leerDb(); // leyendo toda la data que se encuentra en nuestro bd (archivo json)
+    if (tareasDb) {
+
+    }
+    await pausa();
     do {
         opt = await inquirerMenu(); // utilizando la función de inquirer para imprimir el menu
-        
-        switch(opt){
-            case'1':
-            const desc = await leerInput('Descripcion de la tarea:'); // utilizamos la función de leer inputt 
-            tareas.crearTarea(desc); // utilizando el metodo de crear tarea enviamos la descripción de la tarea que ingreso el usaurio
 
-            break;
-            case'2':
-            console.log(tareas.listadoArr); // mostramos el listado de tareas creadas
-            break;
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripcion de la tarea:'); // utilizamos la función de leer inputt 
+                tareas.crearTarea(desc); // utilizando el metodo de crear tarea enviamos la descripción de la tarea que ingreso el usaurio
+
+                break;
+            case '2':
+                console.log(tareas.listadoArr); // mostramos el listado de tareas creadas
+                break;
         }
 
         guardarDb(tareas.listadoArr); // llamamos al metodo de guardar el archivo
-    
+
         await pausa();
     } while (opt !== '0') {
 
--- app-tareas/helpers/guardarArchivo.js
+++ app-tareas/helpers/guardarArchivo.js
@@ -1,12 +1,23 @@
-
 const fs = require('fs');
+
+const archivo = './db/data.json' // la ruta donde se almacenara el archivo
 
 const guardarDb = (data) => {
 
-    const archivo = './db/data.json' // la ruta donde se almacenara el archivo
     fs.writeFileSync(archivo, JSON.stringify(data)); // debemos convertir la data a un string para poder almacenarla
+}
 
+const leerDb = () => { // metodo para leer el archivo json
+    if (!fs.existsSync(archivo)) { // confirmando si el archivo existe
+        return null;
+    }
+    
+    // usando el readFile para la lectura
+    const info = fs.readFileSync(archivo, { encoding: 'utf-8' });
+    const data = JSON.parse(info) // convirtiendo el string de nuevo a un JSON
+    return data // retornando el arreglo de de las tareas
 }
 module.exports = {
-    guardarDb
+    guardarDb,
+    leerDb
 }
